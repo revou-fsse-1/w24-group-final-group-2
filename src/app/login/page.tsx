c8ec3f86b7f77a420eb7368476a953559e9aa3b8
Derian Melvin <derianmelvin88@gmail.com>
2023-07-28 21:34:13 +0700

    Add NextAuth functionality

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import bacgkroundImg from "../../../public/assets/backgroundImg-auth.jpg";
+import { signIn, useSession } from "next-auth/react";
 
-export default function page() {
+export default function Login() {
+  const { data: session } = useSession();
+
   return (
     <section className="min-h-screen flex bg-[#CFD4CB]">
       <div className="flex flex-col justify-between p-8 sm:w-2/3 sm:p-12 lg:max-w-lg">
@@ -11,13 +16,25 @@ export default function page() {
           </div>
 
           <div className="flex flex-col gap-2">
-            <h2 className="text-4xl font-semibold text-[#203D59] sm:text-5xl">Welcome</h2>
+            <h2 className="text-4xl font-semibold text-[#203D59] sm:text-5xl">
+              Welcome
+            </h2>
             <p>Rare and valuable collectibles awaits you</p>
           </div>
 
           <div className="flex flex-col items-center gap-5 text-center">
-            <button className="w-full px-4 py-3 bg-gray-200 rounded-md">Login with Google</button>
-            <button className="w-full px-4 py-3 bg-gray-200 rounded-md">Login with Github</button>
+            <button
+              onClick={() => signIn("google", { callbackUrl: "/" })}
+              className="w-full px-4 py-3 bg-gray-200 rounded-md"
+            >
+              Login with Google
+            </button>
+            <button
+              onClick={() => signIn("github", { callbackUrl: "/" })}
+              className="w-full px-4 py-3 bg-gray-200 rounded-md"
+            >
+              Login with Github
+            </button>
             <p className="text-sm text-gray-500">
               By logging in, you have agreed to MARKILANG
               <strong> Privacy Policy</strong> and
@@ -32,7 +49,11 @@ export default function page() {
       </div>
 
       <div className="hidden items-center relative sm:flex sm:w-2/3 lg:w-full">
-        <Image src={bacgkroundImg} alt="illustration one" className="h-full object-cover" />
+        <Image
+          src={bacgkroundImg}
+          alt="illustration one"
+          className="h-full object-cover"
+        />
         <div className="w-full h-full absolute bg-gradient-to-r from-[#203D59]"></div>
       </div>
     </section>
