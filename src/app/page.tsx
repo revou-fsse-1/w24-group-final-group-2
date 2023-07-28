44762c2acbba90740c1da78344ec01a0bd64360c
Derian Melvin <derianmelvin88@gmail.com>
2023-07-28 21:35:14 +0700

    Add simple NextAuth test example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from 'next/image'
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  const validateUserLogin = () => {
+    return session && session.user ? (
+      <div className="flex flex-col gap-2 p-5 border-2">
+        <p>{session.user.email}</p>
+        <p>{session.user.image}</p>
+        <p>{session.user.name}</p>
+
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    ) : (
+      ""
+    );
+  };
+
   return (
     <main>
+      {validateUserLogin()}
       <h1>Hello World</h1>
     </main>
-  )
+  );
 }
