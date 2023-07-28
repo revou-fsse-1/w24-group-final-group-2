5bbfa2bb5206f37758abf727579654657509db41
mahdiharish <mahdiharish12@gmail.com>
2023-07-29 00:50:37 +0800

    add crud feature on assets

---
+++ src/app/api/assets/[id]/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/libs/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const id = params.id;
+  const asset = await prisma.asset.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  return NextResponse.json(asset);
+}
+
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+  const id = params.id;
+  const json = await request.json();
+
+  const update = await prisma.asset.update({
+    where: {
+      id: id,
+    },
+    data: json
+  });
+  return NextResponse.json(update);
+}
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+  const id = params.id;
+  const json = await request.json();
+
+  const updated = await prisma.asset.update({
+    where: {
+      id: id,
+    },
+    data: json,
+  });
+
+  return NextResponse.json(updated);
+}
---
+++ src/app/api/assets/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/libs/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const asset = await prisma.asset.findMany();
+  console.log(asset);
+  return new Response(JSON.stringify(asset));
+}
+
+export async function POST(request: Request) {
+  const json = await request.json();
+  const post = await prisma.asset.create({
+    data: json,
+  })
+  return NextResponse.json(post);
+}
+
