d3b7d33bfe3231c59301e3abab7e3cfca68c3b60
Derian Melvin <derianmelvin88@gmail.com>
2023-07-29 22:26:58 +0700

    Update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 import LogoBlue from "./logo/LogoBlue";
 import IconAvatar from "./icons/IconAvatar";
-import IconHamburger from "./icons/IconHamburger";
+import Link from "next/link";
+import IconSearch from "./icons/IconSearch";
 
 export default function Header() {
+  const { data: session, status } = useSession();
   const [searchInput, setSearchInput] = useState("");
 
   const searchAuctionList = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -12,35 +15,50 @@ export default function Header() {
     }
   };
 
+  const handleLoginDisplay = () => {
+    return status == "loading" ? (
+      <div>...</div>
+    ) : status == "unauthenticated" ? (
+      <Link href={"/login"}>
+        <button className="flex gap-2">
+          <IconAvatar size={30} />
+          <span>Login</span>
+        </button>
+      </Link>
+    ) : (
+      <button className="flex gap-2">
+        <IconAvatar size={30} />
+        <span>{session?.user?.name}</span>
+      </button>
+    );
+  };
+
   return (
     <div className="w-full flex flex-col items-center px-6 py-6 bg-[#CFD4CB]">
       <header className="w-full max-w-7xl flex items-center justify-between gap-5">
-        <div className="flex gap-3 items-center">
+        <Link href={"/"} className="flex gap-3 items-center">
           <LogoBlue size={55} />
           <span className="text-xl font-bold text-[#203C59]">MARKILANG</span>
-        </div>
+        </Link>
 
-        <div className="w-1/3 hidden md:flex">
+        <div className="w-1/3 hidden md:flex md:relative">
+          <div className="absolute right-1 top-[3px] p-[7px] rounded-full hover:bg-gray-200">
+            <IconSearch size={25} />
+          </div>
           <input
             type="text"
             name="auctionItem"
+            placeholder="Search for..."
             onChange={(e) => setSearchInput(e.target.value)}
             onKeyDown={(e) => searchAuctionList(e)}
-            className="w-full px-5 py-2 text-lg rounded-full"
+            className="w-full pl-5 pr-12 py-2 text-lg rounded-full"
           />
         </div>
 
         <div className="hidden gap-8 text-lg text-[#203C59] md:flex">
           <button>About</button>
           <button>Auctions</button>
-          <div className="flex gap-2">
-            <IconAvatar size={30} />
-            <button>Login</button>
-          </div>
-        </div>
-
-        <div className="flex items-center md:hidden">
-          <IconHamburger size={30} />
+          {handleLoginDisplay()}
         </div>
       </header>
     </div>
