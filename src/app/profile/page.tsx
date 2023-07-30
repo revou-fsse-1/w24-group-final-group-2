2c2e1b3609b228641f3c1fe6e373d1477b98b93a
Derian Melvin <derianmelvin88@gmail.com>
2023-07-30 15:48:06 +0700

    Create profile layout & update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import IconWarning from "@/components/icons/IconWarning";
-import MobileNav from "@/components/MobileNav";
 
 export default function Profile() {
   const phoneRegExp =
@@ -37,104 +34,99 @@ export default function Profile() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-between">
-      <Header />
+    <div className="w-full max-w-7xl flex p-6 ">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full flex flex-col gap-5 text-lg md:text-xl md:w-1/2"
+      >
+        <h1 className="text-4xl font-bold py-4 text-[#203C59] md:text-5xl">
+          My Profile
+        </h1>
 
-      <div className="w-full max-w-7xl flex p-6 ">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full flex flex-col gap-5 text-lg md:text-xl md:w-1/2"
+        <div className="flex flex-col gap-1 md:w-3/4">
+          <label htmlFor="email">Email</label>
+          <span className="font-bold">exampleEmail@google.com</span>
+        </div>
+
+        <div className="flex flex-col gap-1 md:w-3/4">
+          <label htmlFor="username">Username</label>
+          <input
+            {...register("username")}
+            className="px-3 py-2 border border-black rounded-md"
+          />
+          <span className="text-sm text-rose-500">
+            {errors.username?.message}
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-1 md:w-3/4">
+          <label htmlFor="creditAmount">Credit Amount</label>
+          <input
+            type="number"
+            defaultValue={0}
+            {...register("creditAmount")}
+            className="px-3 py-2 border border-black rounded-md"
+          />
+          <span className="text-sm text-rose-500">
+            {errors.creditAmount?.message}
+          </span>
+        </div>
+
+        <hr className="h-[2px] my-4 border-0 bg-[#222E3F] bg-opacity-40" />
+
+        <h2 className="text-2xl font-bold text-[#203C59] md:text-3xl">
+          Delivery Details
+        </h2>
+
+        <div className="flex items-center gap-3 px-4 py-2 text-sm rounded-md bg-[#EAC066] bg-opacity-50 md:w-3/4">
+          <div className="w-fit">
+            <IconWarning size={30} />
+          </div>
+          <p>
+            Please fill in the information below to receive delivery of your
+            assets
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-1 md:w-3/4">
+          <label htmlFor="name">Full Name</label>
+          <input
+            {...register("name")}
+            className="px-3 py-2 border border-black rounded-md"
+          />
+          <span className="text-sm text-rose-500">{errors.name?.message}</span>
+        </div>
+
+        <div className="flex flex-col gap-1 md:w-3/4">
+          <label htmlFor="address">Address</label>
+          <textarea
+            rows={5}
+            {...register("address")}
+            className="resize-none px-3 py-2 border border-black rounded-md"
+          />
+          <span className="text-sm text-rose-500">
+            {errors.address?.message}
+          </span>
+        </div>
+
+        <div className="flex flex-col gap-1 md:w-3/4">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            {...register("phoneNumber")}
+            className="px-3 py-2 border border-black rounded-md"
+          />
+          <span className="text-sm text-rose-500">
+            {errors.phoneNumber?.message}
+          </span>
+        </div>
+
+        <button
+          type="submit"
+          className="w-fit px-8 py-3 rounded-md bg-[#EAC066]"
         >
-          <h1 className="text-4xl font-bold py-4 text-[#203C59] md:text-5xl">
-            My Profile
-          </h1>
-
-          <div className="flex flex-col gap-1 md:w-3/4">
-            <label htmlFor="email">Email</label>
-            <span className="font-bold">exampleEmail@google.com</span>
-          </div>
-
-          <div className="flex flex-col gap-1 md:w-3/4">
-            <label htmlFor="username">Username</label>
-            <input
-              {...register("username")}
-              className="px-3 py-2 border border-black rounded-md"
-            />
-            <span className="text-sm text-rose-500">
-              {errors.username?.message}
-            </span>
-          </div>
-
-          <div className="flex flex-col gap-1 md:w-3/4">
-            <label htmlFor="creditAmount">Credit Amount</label>
-            <input
-              type="number"
-              defaultValue={0}
-              {...register("creditAmount")}
-              className="px-3 py-2 border border-black rounded-md"
-            />
-            <span className="text-sm text-rose-500">
-              {errors.creditAmount?.message}
-            </span>
-          </div>
-
-          <hr className="h-[2px] my-4 border-0 bg-[#222E3F] bg-opacity-40" />
-
-          <div className="flex items-center gap-3 px-4 py-2 text-sm rounded-md bg-[#EAC066] bg-opacity-50 md:w-3/4">
-            <div className="w-fit">
-              <IconWarning size={30} />
-            </div>
-            <p>
-              Please fill in the information below to receive delivery of your
-              assets
-            </p>
-          </div>
-
-          <div className="flex flex-col gap-1 md:w-3/4">
-            <label htmlFor="name">Full Name</label>
-            <input
-              {...register("name")}
-              className="px-3 py-2 border border-black rounded-md"
-            />
-            <span className="text-sm text-rose-500">
-              {errors.name?.message}
-            </span>
-          </div>
-
-          <div className="flex flex-col gap-1 md:w-3/4">
-            <label htmlFor="address">Address</label>
-            <textarea
-              rows={5}
-              {...register("address")}
-              className="resize-none px-3 py-2 border border-black rounded-md"
-            />
-            <span className="text-sm text-rose-500">
-              {errors.address?.message}
-            </span>
-          </div>
-
-          <div className="flex flex-col gap-1 md:w-3/4">
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <input
-              {...register("phoneNumber")}
-              className="px-3 py-2 border border-black rounded-md"
-            />
-            <span className="text-sm text-rose-500">
-              {errors.phoneNumber?.message}
-            </span>
-          </div>
-
-          <button
-            type="submit"
-            className="w-fit px-8 py-3 rounded-md bg-[#EAC066]"
-          >
-            Update Profile
-          </button>
-        </form>
-      </div>
-
-      <MobileNav />
-      <Footer />
+          Update Profile
+        </button>
+      </form>
     </div>
   );
 }
