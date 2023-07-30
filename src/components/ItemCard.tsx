6da5acb7cb7f48a45b7fbecb97da1b12c890b0b7
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-07-30 15:44:17 +0700

    add timer function

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,4 +1,34 @@
-export default function ItemCard() {
+'use client';
+import { useState, useEffect } from 'react';
+interface IItemCard {
+	name: string;
+	endDate: Date;
+}
+export default function ItemCard({ name, endDate }: IItemCard) {
+	// const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+	// useEffect(() => {
+	// 	const timer = setInterval(() => {
+	// 		setTimeLeft(calculateTimeLeft());
+	// 	}, 1000);
+	// 	return () => {
+	// 		clearInterval(timer);
+	// 	};
+	// });
+
+	// function calculateTimeLeft() {
+	// 	const now = new Date();
+	// 	// const diff = endDate.getTime() - now.getTime();
+
+	// 	return {
+	// 		days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+	// 		hours: Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+	// 		minutes: Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)),
+	// 		seconds: Math.floor((diff % (1000 * 60)) / 1000),
+	// 	};
+	// }
+
+	const replacement = new Date();
+
 	return (
 		<div>
 			<img
@@ -6,8 +36,8 @@ export default function ItemCard() {
 				alt="dummy-image"
 				className=" w-[325px] h-[325px]"
 			/>
-			<h3 className="mt-8 text-mkl-secondary">Item Title</h3>
-			<p className="mt-3 ">TIME LEFT: 2 Hours 24 minutes</p>
+			<h3 className="mt-8 text-mkl-secondary">{name}</h3>
+			<p className="mt-3 max-w-[325px]">{`${replacement}`}</p>
 			<button className=" mt-7 w-full max-w-[325px] btn-primary">Bid</button>
 		</div>
 	);
