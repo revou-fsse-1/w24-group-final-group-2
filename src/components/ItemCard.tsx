5077cb11afd08887fd1c4ee522e852570de3753b
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-08-01 02:07:47 +0700

    ItemCard db data adjustment

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,13 +1,23 @@
 'use client';
 import { redirect } from 'next/navigation';
 import { useState, useEffect } from 'react';
+import { indonesianCurrency } from '@/utils/Currency';
 interface IItemCard {
 	id: string;
 	name: string;
 	imageUrl: string;
+	price: number;
+	// highestBid: number;
 	endDate: Date;
 }
-export default function ItemCard({ id, name, imageUrl, endDate }: IItemCard) {
+export default function ItemCard({
+	id,
+	name,
+	imageUrl,
+	price,
+	// highesBids,
+	endDate,
+}: IItemCard) {
 	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 	useEffect(() => {
 		const timer = setInterval(() => {
@@ -32,10 +42,9 @@ export default function ItemCard({ id, name, imageUrl, endDate }: IItemCard) {
 	}
 
 	const replacement = new Date();
-	console.log(imageUrl, 'test');
 
 	return (
-		<div className="transition-all duration-500 hover:scale-110">
+		<div className="transition-all duration-500 hover:scale-110 text-mkl-secondary">
 			<a href={`/auctions/${id}`}>
 				<img
 					src={`${imageUrl}`}
@@ -43,9 +52,16 @@ export default function ItemCard({ id, name, imageUrl, endDate }: IItemCard) {
 					className=" aspect-square max-w-[325px] aspect-square overflow-hidden"
 				/>
 			</a>
-			<h3 className="mt-8 max-w-[325px] text-mkl-secondary">{name}</h3>
-			<p className="mt-3 max-w-[325px] text-mkl-secondary">
-				<b>Time Left:</b>
+			<h3 className="mt-8 max-w-[325px]">{name}</h3>
+			<p className="mt-3">
+				<small>
+					Start: {indonesianCurrency.format(price)}
+					<br />
+				</small>
+				<b>{indonesianCurrency.format(price)}</b>
+			</p>
+			<p className="mt-3 max-w-[325px]">
+				<b>Time Left: </b>
 				{`${timeLeft.days}d ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}
 			</p>
 			<a href={`/auctions/${id}`}>
