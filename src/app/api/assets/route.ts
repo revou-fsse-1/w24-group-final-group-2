9125a98781c0c8ce13427f0e61664960cee3a7a0
Rafael PraditaMac <rafaeljonathan.work@gmail.com>
2023-07-31 14:03:50 +0700

    add asset back end

--- src/app/api/assets/route.ts
+++ src/app/api/assets/route.ts
@@ -1,17 +1,16 @@
-import { prisma } from "@/libs/db";
-import { NextResponse } from "next/server";
+import { prisma } from '@/libs/db';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const asset = await prisma.asset.findMany();
-  console.log(asset);
-  return new Response(JSON.stringify(asset));
+	const asset = await prisma.asset.findMany();
+	console.log(asset);
+	return new Response(JSON.stringify(asset));
 }
 
 export async function POST(request: Request) {
-  const json = await request.json();
-  const post = await prisma.asset.create({
-    data: json,
-  })
-  return NextResponse.json(post);
+	const json = await request.json();
+	const post = await prisma.asset.create({
+		data: json,
+	});
+	return NextResponse.json(post);
 }
-
