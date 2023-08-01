f01145a8388fb2bce116d71462194f5744796d1f
Rafael PraditaMac <rafaeljonathan.work@gmail.com>
2023-08-01 17:29:56 +0700

    data fetch test done

--- src/app/auctions/[id]/page.tsx
+++ src/app/auctions/[id]/page.tsx
@@ -1,17 +1,37 @@
+'use client';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import useSWR from 'swr';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
+
+async function fetcher(url: string) {
+	try {
+		const res = await axios.get(url);
+		return res.data;
+	} catch (error) {
+		throw new Error('error fetching data');
+	}
+}
 
 export default function AuctionPage() {
+	const params = useParams();
+	const { data, error, isLoading } = useSWR(
+		`/api/assets/${params.id}`,
+		fetcher
+	);
 	return (
 		<>
 			<Header />
 			<main>
 				<div>
-					<img />
+					<img src={`${data.imageUrl}`} alt="" />
+					<p></p>
 				</div>
 				<div>
-					<h1>Product Name</h1>
-					<p>Description</p>
+					<h1>{data.name}</h1>
+					<p></p>
+					<p>{data.description}</p>
 				</div>
 				<div>
 					<p>open Price - Highest Bids</p>
