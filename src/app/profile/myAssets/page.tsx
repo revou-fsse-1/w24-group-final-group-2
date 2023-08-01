138756679702f76322d18bc81fe8c624dbe833fc
Derian Melvin <derianmelvin88@gmail.com>
2023-08-01 19:44:17 +0700

    Update myAssets page

--- src/app/profile/myAssets/page.tsx
+++ src/app/profile/myAssets/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import LoadingSpinner from "@/components/LoadingSpinner";
 import MyAssetCard from "@/components/profile/MyAssetCard";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import useSWR from "swr";
 
 const sampleData = [
   {
@@ -22,12 +27,55 @@ const sampleData = [
 ];
 
 export default function MyAssets() {
+  const { data: session, status } = useSession();
+
+  if (status == "unauthenticated") {
+    redirect("/login");
+  }
+
+  // Fetching data
+  const fetchData = (url: string) =>
+    axios
+      .get(url)
+      .then((data) => {
+        return data.data;
+      })
+      .catch((error) => {
+        throw new Error("Failed fetching data");
+      });
+  const { data, isLoading } = useSWR("/api/users/assets", fetchData);
+
+  if (!isLoading) {
+    console.log(data);
+  }
+
+  const displayMyAssets = () => {
+    return data.map((asset: any) => (
+      <MyAssetCard
+        key={asset.id}
+        id={asset.id}
+        imageUrl={asset.imageUrl}
+        assetName={asset.name}
+        endTime={asset.endTime}
+        startingPrice={asset.openingPrice}
+        currentPrice={!asset.bidAssets[0] ? 0 : asset.bidAssets[0].currentPrice}
+      />
+    ));
+  };
+
   return (
     <div className="w-full max-w-7xl flex flex-col p-6">
       <h1 className="text-4xl font-bold py-4 text-[#203C59] md:text-5xl">
         My Assets
       </h1>
-      {sampleData.map((data) => (
+      {!data ? (
+        <div className="w-full flex items-center justify-center">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        displayMyAssets()
+      )}
+      {/* {sampleData.map((data) => (
         <MyAssetCard
           key={data.id}
           id={data.id}
@@ -37,7 +85,7 @@ export default function MyAssets() {
           startingPrice={data.startingPrice}
           currentPrice={data.currentPrice}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
