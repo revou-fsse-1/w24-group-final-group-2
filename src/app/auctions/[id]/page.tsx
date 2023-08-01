bc0852b8af176458b5dd1899254dbeb5167aaa82
PapaPanda-Fedora <rafaeljonathan.work@gmail.com>
2023-08-02 03:22:36 +0700

    improve data fetching, add loading state, get user id

--- src/app/auctions/[id]/page.tsx
+++ src/app/auctions/[id]/page.tsx
@@ -2,8 +2,10 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import useSWR from 'swr';
+import { useSession } from 'next-auth/react';
 import { useParams } from 'next/navigation';
 import axios from 'axios';
+import BidCard, { IBid } from '@/components/Auctions/BidCard';
 
 async function fetcher(url: string) {
 	try {
@@ -15,11 +17,24 @@ async function fetcher(url: string) {
 }
 
 export default function AuctionPage() {
+	const { data: session, status } = useSession();
+
 	const params = useParams();
 	const { data, error, isLoading } = useSWR(
 		`/api/assets/${params.id}`,
 		fetcher
 	);
+
+	if (isLoading) {
+		return <div>loading...</div>;
+	}
+
+	const isSeller = data.sellerId === session?.user.id;
+	function handleSubmit() {
+		//check if value is bigger than the highest bid
+		//create the create with axios
+	}
+
 	return (
 		<>
 			<Header />
@@ -34,14 +49,26 @@ export default function AuctionPage() {
 					<p>{data.description}</p>
 				</div>
 				<div>
-					<p>open Price - Highest Bids</p>
-					{/* map this data */}
-					<div>
-						<p>username</p>
-						<p>created at</p>
-						<p>
-							<b>Bid Price</b>
-						</p>
+					<div className={`${isSeller ? 'hidden' : ''}`}>
+						<input type="input" placeholder="Please place your bid!" />
+						<button className="btn-primary" onClick={handleSubmit}>
+							Place Bid!
+						</button>
+					</div>
+					<div className="mt-5">
+						{data.bidAssets.map((bid: IBid) => {
+							return (
+								<>
+									<BidCard
+										key={bid.id}
+										id={bid.id}
+										bidAmount={bid.bidAmount}
+										bidder={bid.bidder}
+										createdAt={bid.createdAt}
+									/>
+								</>
+							);
+						})}
 					</div>
 				</div>
 			</main>
