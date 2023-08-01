e0ccb6c082a29dc0c2241768a2d940437d14b6a5
Rafael PraditaMac <rafaeljonathan.work@gmail.com>
2023-08-01 17:30:41 +0700

    prettier formatting

--- src/app/api/assets/[id]/route.ts
+++ src/app/api/assets/[id]/route.ts
@@ -1,39 +1,48 @@
-import { prisma } from "@/libs/db";
-import { NextResponse } from "next/server";
+import { prisma } from '@/libs/db';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const id = params.id;
-  const asset = await prisma.asset.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  return NextResponse.json(asset);
+export async function GET(
+	request: Request,
+	{ params }: { params: { id: string } }
+) {
+	const id = params.id;
+	const asset = await prisma.asset.findUnique({
+		where: {
+			id: id,
+		},
+	});
+	return NextResponse.json(asset);
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-  const id = params.id;
-  const json = await request.json();
+export async function PUT(
+	request: Request,
+	{ params }: { params: { id: string } }
+) {
+	const id = params.id;
+	const json = await request.json();
 
-  const update = await prisma.asset.update({
-    where: {
-      id: id,
-    },
-    data: json
-  });
-  return NextResponse.json(update);
+	const update = await prisma.asset.update({
+		where: {
+			id: id,
+		},
+		data: json,
+	});
+	return NextResponse.json(update);
 }
 
-export async function PATCH(request: Request, { params }: { params: { id: string } }) {
-  const id = params.id;
-  const json = await request.json();
+export async function PATCH(
+	request: Request,
+	{ params }: { params: { id: string } }
+) {
+	const id = params.id;
+	const json = await request.json();
 
-  const updated = await prisma.asset.update({
-    where: {
-      id: id,
-    },
-    data: json,
-  });
+	const updated = await prisma.asset.update({
+		where: {
+			id: id,
+		},
+		data: json,
+	});
 
-  return NextResponse.json(updated);
+	return NextResponse.json(updated);
 }
