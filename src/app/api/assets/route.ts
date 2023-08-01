fdb204e2d8d2f14980c651fc1b00637dc7156aad
Rafael PraditaMac <rafaeljonathan.work@gmail.com>
2023-08-01 14:43:54 +0700

    adjust asset api route

--- src/app/api/assets/route.ts
+++ src/app/api/assets/route.ts
@@ -1,10 +1,47 @@
 import { prisma } from '@/libs/db';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET() {
-	const asset = await prisma.asset.findMany();
-	console.log(asset);
-	return new Response(JSON.stringify(asset));
+// export async function GET() {
+// 	const asset = await prisma.asset.findMany();
+// 	console.log(asset);
+// 	return new Response(JSON.stringify(asset));
+// }
+
+export async function GET(req: NextRequest) {
+	const page = req.nextUrl.searchParams.get('page');
+	const limit = req.nextUrl.searchParams.get('limit');
+	const search = req.nextUrl.searchParams.get('search');
+
+	const pageNumber = parseInt(page as string);
+	const perPageNumber = parseInt(limit as string);
+	try {
+		const whereClause = {
+			OR: [
+				{
+					name: {
+						contains: search,
+						mode: 'insensitive',
+					},
+				},
+				{
+					description: {
+						contains: search,
+						mode: 'insensitive',
+					},
+				},
+			],
+		};
+		const assets = await prisma.asset.findMany({
+			where: search ? whereClause : {},
+			skip: (pageNumber - 1) * perPageNumber,
+			take: perPageNumber,
+		});
+		return NextResponse.json({
+			assets,
+		});
+	} catch (error) {
+		console.error('Error fecthing data', error);
+	}
 }
 
 export async function POST(request: Request) {
