c1eeccb379f4ddd692f9c1f4307c0a0ead163e73
Derian Melvin <derianmelvin88@gmail.com>
2023-08-01 19:41:41 +0700

    Update users/[email] api

--- src/app/api/users/[email]/route.ts
+++ src/app/api/users/[email]/route.ts
@@ -11,19 +11,6 @@ export async function GET(request: Request, { params }: { params: { email: strin
   return NextResponse.json(user);
 }
 
-export async function PUT(request: Request, { params }: { params: { email: string } }) {
-  const email = params.email;
-  const json = await request.json();
-
-  const update = await prisma.user.update({
-    where: {
-      email: email,
-    },
-    data: json,
-  });
-  return NextResponse.json(update);
-}
-
 export async function PATCH(request: Request, { params }: { params: { email: string } }) {
   const email = params.email;
   const json = await request.json();
