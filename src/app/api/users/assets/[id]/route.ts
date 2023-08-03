ab68cfe6dec661f815abc09c06e025f09e5b6e65
Nofrialdi <aldi.fasive@gmail.com>
2023-08-03 21:51:38 +0700

    add nextrequest aasetId and bidsId route

--- src/app/api/users/assets/[id]/route.ts
+++ src/app/api/users/assets/[id]/route.ts
@@ -1,53 +1,50 @@
-import { prisma } from "@/libs/db";
-import { NextResponse } from "next/server";
+import { prisma } from '@/libs/db';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET({ params }: { params: { id: string } }) {
-  const id = params.id;
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+	const id = params.id;
 
-  const asset = await prisma.asset.findFirst({
-    where: {
-      id: id,
-    },
-    select: {
-      id: true,
-      name: true,
-      description: true,
-      openingPrice: true,
-      endTime: true,
-      imageUrl: true,
-    },
-  });
-  return NextResponse.json(asset);
+	const asset = await prisma.asset.findFirst({
+		where: {
+			id: id,
+		},
+		select: {
+			id: true,
+			name: true,
+			description: true,
+			openingPrice: true,
+			endTime: true,
+			imageUrl: true,
+		},
+	});
+	return NextResponse.json(asset);
 }
 
-export async function PATCH(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const id = params.id;
-  const json = await request.json();
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+	const id = params.id;
+	const json = await request.json();
 
-  const updateAsset = await prisma.asset.update({
-    where: {
-      id: id,
-    },
-    data: json,
-  });
+	const updateAsset = await prisma.asset.update({
+		where: {
+			id: id,
+		},
+		data: json,
+	});
 
-  return NextResponse.json(updateAsset);
+	return NextResponse.json(updateAsset);
 }
 
-export async function DELETE({ params }: { params: { id: string } }) {
-  const id = params.id;
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+	const id = params.id;
 
-  const deleteAsset = await prisma.asset.update({
-    where: {
-      id: id,
-    },
-    data: {
-      deletedAt: new Date().toISOString(),
-    },
-  });
+	const deleteAsset = await prisma.asset.update({
+		where: {
+			id: id,
+		},
+		data: {
+			deletedAt: new Date().toISOString(),
+		},
+	});
 
-  return NextResponse.json(deleteAsset);
+	return NextResponse.json(deleteAsset);
 }
