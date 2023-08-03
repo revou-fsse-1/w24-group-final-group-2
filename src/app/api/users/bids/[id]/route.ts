a53272b84ef66899c7f8db2ea83518d3f2160e81
Nofrialdi <aldi.fasive@gmail.com>
2023-08-03 21:51:38 +0700

    add nextrequest aasetId and bidsId route

--- src/app/api/users/bids/[id]/route.ts
+++ src/app/api/users/bids/[id]/route.ts
@@ -1,39 +1,39 @@
-import { prisma } from "@/libs/db";
-import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { prisma } from '@/libs/db';
+import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
-export async function GET({ params }: { params: { id: string } }) {
-  const session = await getServerSession(authOptions);
-  const id = params.id;
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+	const session = await getServerSession(authOptions);
+	const id = params.id;
 
-  // Get user bid by id
-  const bidList = await prisma.bidAsset.findFirst({
-    where: {
-      bidder: {
-        email: session?.user?.email?.toString(),
-      },
-      id: id,
-    },
-    select: {
-      id: true,
-      bidAmount: true,
-      asset: {
-        select: {
-          id: true,
-          name: true,
-        },
-      },
-      bidder: {
-        select: {
-          name: true,
-          address: true,
-          phoneNumber: true,
-          creditAmount: true,
-        },
-      },
-    },
-  });
+	// Get user bid by id
+	const bidList = await prisma.bidAsset.findFirst({
+		where: {
+			bidder: {
+				email: session?.user?.email?.toString(),
+			},
+			id: id,
+		},
+		select: {
+			id: true,
+			bidAmount: true,
+			asset: {
+				select: {
+					id: true,
+					name: true,
+				},
+			},
+			bidder: {
+				select: {
+					name: true,
+					address: true,
+					phoneNumber: true,
+					creditAmount: true,
+				},
+			},
+		},
+	});
 
-  return NextResponse.json(bidList);
+	return NextResponse.json(bidList);
 }
